d0779584e135793ed5dd4b7d69a4f5cfa4476235
Ravi Prakash Chaurasiya <113404805+rp7007@users.noreply.github.com>
2023-06-24 12:19:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Function to fetch data from the API
+// Function to fetch the data from the API
 function fetchData() {
     const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
     
